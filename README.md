4773fca5eeaf1af0d5daf52f2686ccd75c374baf
Matteo Bruni <matteo.bruni@eudata.com>
2018-11-21 17:32:19 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # pm2-logwrapper
-log wrapper around console.log / console.error to implements different log levels
+log wrapper around console.log / console.error to implements different log levels when used with pm2
+
+> Create logger
+
+```js
+const logger = require('pm2-logwrapper');
+```
+
+> By default the environment variable used to verify the log level enabled is `LOG_LEVEL`. You can change the default with the init option `env_var`. 
+
+```js
+logger.init({
+     env_var: "MY_CUSTOM_ENV_VAR"
+});
+```
+
+> By default error and fatal appenders are only on `console.error`. If you want you can append these loggers also on console.log with `errors_on_out` 
+
+```js
+logger.init({
+     errors_on_out: false
+});
+```
+
+Both options can be changed runtime
---
+++ index.js
@@ -0,0 +1,97 @@
+var opts = {
+    env_var : "LOG_LEVEL",
+    errors_on_out: false
+};
+
+var levels = [
+    "off",
+    "fatal",
+    "error",
+    "warn",
+    "info",
+    "debug",
+    "trace"
+];
+
+
+module.exports = {
+    init : function(options) {
+        opts = Object.assign(opts, options);
+    },
+
+    trace : function(message) {
+        return log("trace", message);
+    },
+    isTraceEnabled : function() {
+        return isLevelEnabled("trace");
+    },
+
+    debug : function(message) {
+        return log("debug", message);
+    },
+    isDebugEnabled : function() {
+        return isLevelEnabled("debug");
+    },
+
+    info : function(message) {
+        return log("info", message);
+    },
+    isInfoEnabled : function() {
+        return isLevelEnabled("info");
+    },
+
+    warn : function(message) {
+        return log("warn", message);
+    },
+    isWarnEnabled : function() {
+        return isLevelEnabled("warn");
+    },
+
+    error : function(message) {
+        let tmp = true;
+        if (opts.errors_on_out)
+            tmp = log("error", message);
+        return tmp && error("error", message);
+    },
+    isErrorEnabled : function() {
+        return isLevelEnabled("error");
+    },
+
+    fatal : function(message) {
+        let tmp = true;
+        if (opts.errors_on_out)
+            tmp = log("fatal", message);
+        return tmp && error("error", message);
+    },
+    isFatalEnabled : function() {
+        return isLevelEnabled("fatal");
+    }
+};
+
+function isLevelEnabled(level) {
+    return getLevel()>=levels.indexOf(level);
+}
+
+function log(level, message) {
+    if (isLevelEnabled(level)) {
+        console.log(message);
+        return true;
+    }
+    return false;
+}
+
+function error(level, message) {
+    if (getLevel()>=levels.indexOf(level)) {
+        console.error(message);
+        return true;
+    }
+    return false;
+}
+
+function getLevel() {
+    var level = "info";
+    if (process.env[opts.env_var]) {
+        level = process.env[opts.env_var].toLowerCase();
+    }
+    return levels.indexOf(level);
+}
